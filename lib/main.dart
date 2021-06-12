b3db38db90327878ebf8b0fa46b8e4bc7b2b2152
Bara C. Tudor <vector2025@gmail.com>
2021-06-12 21:10:03 +0300

    adauga font Rubik

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Planneroo',
+      //theme: ThemeData(fontFamily: 'Rubik'),
       home: Container(
         child: HomePage()
       ),
